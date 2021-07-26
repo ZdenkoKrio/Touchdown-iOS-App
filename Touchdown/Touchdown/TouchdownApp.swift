5bc027fdbd65b9c6ef73aa03273489d0bacff822
Zdenko Čepan <37399415+ZdenkoKrio@users.noreply.github.com>
2021-07-26 19:53:47 +0200

    created new project

---
+++ Touchdown/Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Zdenko Čepan on 26/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
