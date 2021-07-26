ad548295dd57d46aa4951cd3424bde360a0dafa4
Zdenko Čepan <37399415+ZdenkoKrio@users.noreply.github.com>
2021-07-26 23:36:16 +0200

    created footerView

---
+++ Touchdown/Touchdown/Utility/Constant.swift
@@ -0,0 +1,28 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Zdenko Čepan on 26/07/2021.
+//
+
+import SwiftUI
+
+// DATA
+
+// COLOR
+let colorBackground: Color = Color("colorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// LAYOUT
+
+// UX
+
+// API
+
+// IMAGE
+
+// FONT
+
+// STRING
+
+// MISC
