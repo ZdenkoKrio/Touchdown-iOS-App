15239d6d289872cc7f33376d17f1c7dce6011ea6
Zdenko Čepan <37399415+ZdenkoKrio@users.noreply.github.com>
2021-07-28 17:57:15 +0200

    created custom NavBarView

---
+++ Touchdown/Touchdown/App/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  Touchdown
+//
+//  Created by Zdenko Čepan on 26/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    // MARK: - Properties
+    
+    // MARK: - Body
+    var body: some View {
+        ZStack {
+            VStack(spacing: 0) {
+                NavigationBarView()
+                    .padding(.horizontal, 15)
+                    .padding(.bottom)
+                    .padding(.top,
+                             UIApplication.shared.windows.first?.safeAreaInsets.top)
+                    .background(Color.white)
+                    .shadow(color: Color.black.opacity(0.10), radius: 10, x: 0, y: 10)
+                Spacer()
+                
+                FooterView()
+                    .padding(.horizontal)
+            } // VSTACK
+            .background(colorBackground.ignoresSafeArea(.all, edges: .all))
+        } // ZSTACK
+        .ignoresSafeArea(.all, edges: .top)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
